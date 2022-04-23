abe29e425f16b930e983f118a045129463692164
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2022-04-22 19:31:38 -0500

    Implement function to get data and loader component in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllPokemonsData } from '../../Functions';
+
+import Loader from '../Loader/Loader';
 
 function Home() {
+
+  const [pokemonsData, setPokemonsData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchPokemonsData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const fetchPokemonsData = async () => {
+    try {
+      setLoading(true);
+      
+      let allPokemonsData = await getAllPokemonsData()
+      
+      setPokemonsData(allPokemonsData)
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <Loader />
+    )
+  }
+
   return (
     <>
       <p>Home</p>
